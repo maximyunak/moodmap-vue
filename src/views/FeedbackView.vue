dd3583a6c3baae163d2475168f2e6d951289e086
maximyunak <kleok0039@gmail.com>
2025-11-01 18:25:41 +0300

    feat: поиск отзывов

--- src/views/FeedbackView.vue
+++ src/views/FeedbackView.vue
@@ -7,12 +7,19 @@ const route = useRoute()
 
 const feedbacks = ref([])
 
-const get = async () => {
+const get = async (search = '') => {
   feedbacks.value = (
     await $fetch('/feedbacks', 'get', {
       page: route.query.page || 1,
+      location_id: search,
     })
   ).data
+}
+
+const debounceGet = (search) => {
+  setTimeout(() => {
+    get(search)
+  }, 500)
 }
 
 get()
@@ -23,19 +30,28 @@ watch(
 </script>
 
 <template>
-  <div class="cards">
-    <div class="card" v-for="item in feedbacks.items" :key="item.id">
-      <h3>{{ item?.id }}. {{ item?.emotion }}</h3>
-      <p>{{ item?.comment }}</p>
+  <div>
+    <h2>Все отзывы</h2>
+    <!--    <form @submit.prevent="submit">-->
+    <div class="form-block">
+      <label for="form-search">Поиск по номеру локации</label>
+      <input @change="(e) => debounceGet(e.target.value)" type="text" placeholder="loaction_id" />
     </div>
-  </div>
-  <div class="paginate">
-    <RouterLink
-      :to="`/feedbacks?page=${id + 1}`"
-      :key="id"
-      class="page"
-      v-for="(_, id) in new Array(feedbacks.total_pages)"
-      >{{ id + 1 }}</RouterLink
-    >
+    <!--    </form>-->
+    <div class="cards">
+      <div class="card" v-for="item in feedbacks.items" :key="item.id">
+        <h3>{{ item?.id }}. {{ item?.emotion }}</h3>
+        <p>{{ item?.comment }}</p>
+      </div>
+    </div>
+    <div class="paginate">
+      <RouterLink
+        :to="`/feedbacks?page=${id + 1}`"
+        :key="id"
+        class="page"
+        v-for="(_, id) in new Array(feedbacks.total_pages)"
+        >{{ id + 1 }}</RouterLink
+      >
+    </div>
   </div>
 </template>
